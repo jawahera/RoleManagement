e956917334392e560cbf9b6e2b8c25282f172dc4
jawaher <you@example.com>
2022-10-21 05:28:27 -0400

    initial commit

---
+++ src/test/java/com/Viamobile/RoleManagement/RoleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Viamobile.RoleManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
